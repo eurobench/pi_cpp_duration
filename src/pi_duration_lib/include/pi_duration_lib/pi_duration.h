6cbf3c1c677929b4a0fc46d1ce2e064778ef2362
Alfonso Dominguez <alfonso.dominguez@tecnalia.com>
2020-03-11 13:16:23 +0100

    headers added

--- src/pi_duration_lib/include/pi_duration_lib/pi_duration.h
+++ src/pi_duration_lib/include/pi_duration_lib/pi_duration.h
@@ -1,8 +1,37 @@
+/**
+ * @file   pi_duration.h
+ * @author Alfonso Domínguez <alfonso.dominguez@tecnalia.com>
+ * @date   2020
+ *
+ * Copyright 2020 Tecnalia Research & Innovation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @brief Class with a method which computes an example of a simple 
+ * Performance Indicator (duration of a trial in this case)
+ */
+
 #include <string>
 
 class PIDuration
 {
   public:
     PIDuration();
+
+    /**
+    * @brief Computes the duration of a trial from a file
+    * @param filename name of the file which contains the data about the trial
+    * @return duration of a trial
+    */
     double computeDuration(std::string filename);
 };
--- src/pi_main.cpp
+++ src/pi_main.cpp
@@ -1,3 +1,29 @@
+/**
+ * @file   pi_main.cpp
+ * @author Alfonso Domínguez <alfonso.dominguez@tecnalia.com>
+ * @date   2020
+ *
+ * Copyright 2020 Tecnalia Research & Innovation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @brief Class which contains the main. It takes two input arguments (the 
+ * name of the file which contains tha data of a trial and the name of the 
+ * folder in which the results will be stored), calls a method of the 
+ * library which computes the metrics and stores the result in the folder 
+ * provided as parameter
+ */
+
 #include <pi_duration_lib/pi_duration.h>
 
 #include <filesystem>
@@ -17,7 +43,7 @@ int main (int argc, char *argv[])
 {
   if(argc != 3)
   {
-    std::cerr << "Wrong input parameters!:" << argc << std::endl;
+    std::cerr << "Wrong number of input parameters! The app needs 2 (an input file and an output folder) and you provide " << argc << std::endl;
     return 1;
   }
 
